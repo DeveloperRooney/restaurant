198619fe35995bfa906ead5cf805d73d1d36d7fc
DeveloperRooney <han_0427@naver.com>
2021-08-09 23:11:04 +0900

    레스토랑 예약 관리 시스템 리포지터리 생성

---
+++ src/test/java/com/sh/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
