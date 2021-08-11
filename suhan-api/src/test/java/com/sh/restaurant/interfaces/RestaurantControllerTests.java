6d101089cdc2bdc4599ab9c381a7ca6697ba5c24
DeveloperRooney <han_0427@naver.com>
2021-08-11 10:54:13 +0900

    @MokBean 가짜 객체를 이용한 테스트

--- suhan-api/src/test/java/com/sh/restaurant/interfaces/RestaurantControllerTests.java
+++ suhan-api/src/test/java/com/sh/restaurant/interfaces/RestaurantControllerTests.java
@@ -2,21 +2,26 @@ package com.sh.restaurant.interfaces;
 
 import com.sh.restaurant.application.RestaurantService;
 import com.sh.restaurant.domain.MenuItemRepositoryImpl;
+import com.sh.restaurant.domain.RestaurantDto;
 import com.sh.restaurant.domain.RestaurantRepository;
 import com.sh.restaurant.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(RestaurantController.class)
@@ -25,17 +30,17 @@ class RestaurantControllerTests {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
-
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepositoryImpl menuItemRepository;
 
     @Test
     public void list() throws Exception {
+
+        List<RestaurantDto> restaurants = new ArrayList<>();
+        restaurants.add(new RestaurantDto(1L, "yonan", "Daejeon"));
+
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
 
         mvc.perform(get("/restaurant"))
             .andExpect(status().isOk())
